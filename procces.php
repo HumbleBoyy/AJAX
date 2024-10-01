04d99b80aeda002cf8a7d26f59f53915ea8b6371
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2024-10-01 20:41:22 +0500

    New Commit

--- procces.php
+++ procces.php
@@ -1,4 +1,14 @@
 <?php
 
 
-echo 'Processing...'
+echo 'Processing...';
+
+// Check for POST variable
+if(isset($_POST['name'])){
+    echo 'GET: Your name is'. $_POST['name'];
+}
+
+// Check for Get variable
+if(isset($_GET['name'])){
+    echo 'GET: Your name is'. $_GET['name'];
+}
